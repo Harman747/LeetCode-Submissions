06a05cb201d392c3a5658f23da2b4573c0ce19d9
Harman747 <harman.cu12@gmail.com>
2026-07-14 22:14:03 +0530

    Time: 0 ms (100%), Space: 21.6 MB (94.15%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int maxi = INT_MIN;
-        for(int i = 0 ; i < nums.size() ; i++){
-            maxi = max(maxi , nums[i]);
-        }
-        vector<int> hihi(maxi + 1 , 0);
+        int xr = nums.size();
 
         for(int i = 0 ; i < nums.size() ; i++){
-            hihi[nums[i]]++;
+            xr ^= i;
+            xr ^= nums[i];
         }
-
-        int ans = maxi + 1;
-
-        for(int i = 0 ; i < hihi.size() ; i++){
-            if(hihi[i] == 0){
-                ans = i;
-                break;
-            }
-        }
-        return ans;
+        return xr;
     }
 };
