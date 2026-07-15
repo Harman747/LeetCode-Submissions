976727458da4701fff94a70d0fa1a11225a7281c
Harman747 <harman.cu12@gmail.com>
2026-07-15 19:29:25 +0530

    Time: 0 ms (100%), Space: 8.7 MB (9.11%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
+
+    int gcd(int a , int b){
+        if(b == 0) return a;
+        return gcd(b , a % b);
+    }
+
     int gcdOfOddEvenSums(int n) {
-        return n;
+        if(n == 1){
+            return 1;
+        }
+
+        int odd_sum = (n * n);
+        int even_sum = (n*(n+1));
+
+        return gcd(odd_sum , even_sum);
+
     }
 };
