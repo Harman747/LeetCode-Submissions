bfd70afabad7a96ff05f7a6e8981a739a4209e99
Harman747 <harman.cu12@gmail.com>
2026-07-15 19:33:07 +0530

    Time: 0 ms (100%), Space: 8.7 MB (9.11%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,23 +1,6 @@
 class Solution {
 public:
-    
-    // we can also return n directly as from the formula of odd_sum and even_sum we can see only n is the common factor
-    // se we can also return n directly
-
-    int gcd(int a , int b){
-        if(b == 0) return a;
-        return gcd(b , a % b);
-    }
-
     int gcdOfOddEvenSums(int n) {
-        if(n == 1){
-            return 1;
-        }
-
-        int odd_sum = (n * n);
-        int even_sum = (n*(n+1));
-
-        return gcd(odd_sum , even_sum);
-
+        return n;
     }
 };
