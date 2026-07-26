8cee860ce7c0b5a87a8471b5a5b93ec67ba979ab
Harman747 <harman.cu12@gmail.com>
2026-07-27 00:12:22 +0530

    Time: 0 ms (100%), Space: 31.5 MB (76.45%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -1,13 +1,37 @@
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
-        if(nums.size() < 3) return 1;
-        sort(nums.begin() , nums.end());
-        
-        int n = nums.size();
 
-        int first_min = nums[0] , second_min = nums[1] , third_min = nums[2] , first_max = nums[n - 1], second_max = nums[n - 2] , third_max = nums[n - 3];
+        int f_min = INT_MAX , s_min = INT_MAX , f_max = INT_MIN , s_max = INT_MIN , t_max = INT_MIN ;
 
-        return max((first_min * second_min * first_max) , (first_max * second_max * third_max));
+        for(int i = 0 ; i < nums.size() ; i++){
+            int d = nums[i];
+
+            if(d >= f_max){
+                t_max = s_max;
+                s_max = f_max;
+                f_max = d;
+            }
+
+            else if(d >= s_max){
+                t_max = s_max;
+                s_max = d;
+            }
+
+            else if(d > t_max){
+                t_max = d;
+            }
+
+            if(d <= f_min){
+                s_min = f_min;
+                f_min = d;
+            }
+
+            else if(d < s_min){
+                s_min = d;
+            }
+
+        }
+        return max((s_min * f_min * f_max) , (f_max * s_max * t_max));
     }
 };
