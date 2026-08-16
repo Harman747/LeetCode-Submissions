1637cfc9d7bd8d64b8ba2908b99fda53208bc9de
Harman747 <harman.cu12@gmail.com>
2026-08-16 22:31:01 +0530

    Time: 5 ms (48.73%), Space: 131.3 MB (33.5%) - LeetHub

--- 2029-stone-game-ix/2029-stone-game-ix.cpp
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -6,10 +6,6 @@ public:
         for (int x : stones)
             cnt[x % 3]++;
 
-        // Alice can win if:
-        // 1. cnt[0] is even and both cnt[1] and cnt[2] are non-zero
-        // 2. cnt[0] is odd and the difference between cnt[1] and cnt[2]
-        //    is greater than 2
         if (cnt[0] % 2 == 0)
             return cnt[1] > 0 && cnt[2] > 0;
 
