7837190d87e46f9051575aad505e85ded46d2d58
Harman747 <harman.cu12@gmail.com>
2026-08-18 12:19:29 +0530

    Time: 4 ms (46.24%), Space: 29.7 MB (40.6%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
@@ -4,8 +4,9 @@ public:
 
         unordered_map<int,int> freq;
         unordered_map<int,int> subarray_count;
+        int n = nums.size();
 
-        for(int i = 0 ; i < nums.size() ; i++){
+        for(int i = 0 ; i < n ; i++){
             freq[nums[i]]++;
         }
 
@@ -20,24 +21,26 @@ public:
             return ans;
         }
 
-        if(k == nums.size()){
+        if(k == n){
             for(auto it : freq){
                 ans = max(ans , it.first);
             }
             return ans;
         }
 
-        int f1 = freq[nums[0]] , f2 = freq[nums[nums.size() - 1]];
+        int f1 = freq[nums[0]] , f2 = freq[nums[n - 1]];
+
+        if((freq[nums[0]] == n) && k != nums.size()) return -1;
 
         if(f1 == 1 && f2 != 1){
             return nums[0];
         }
 
         if(f1 != 1 && f2 == 1){
-            return nums[nums.size() - 1];
+            return nums[n-1];
         }
 
-        if((freq[nums[0]] == nums.size()) && k != nums.size()) return -1;
+        if(f1 != 1 && f2 != 1) return -1;
 
         return max(nums[0] , nums[nums.size() - 1]);
         
