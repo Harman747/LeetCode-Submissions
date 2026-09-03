80da6a4afbd6cabb9ca848e17a423b03dd937660
Harman747 <harman.cu12@gmail.com>
2026-09-03 22:03:53 +0530

    Time: 0 ms (100%), Space: 165.7 MB (99.48%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -1,33 +1,42 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        int smallest_odd = INT_MAX;
+
+        int mini = INT_MAX;
+        bool odd_occured = false;
 
         for(int i = 0 ; i < nums1.size() ; i++){
-            if(nums1[i] % 2 != 0){
-                smallest_odd = min(smallest_odd , nums1[i]);
-            }
+            mini = min(mini , nums1[i]);
+            if(nums1[i] % 2 != 0) odd_occured = true;
         }
         
-        bool all_odd = true , all_even = true;
 
-        
-        //all odd try by subtract smallest even as (even - odd) = (odd - even) = odd number
 
-        //all even try by subtract smallest even if num is even or subtract odd if num is odd as (even - even) = (odd - odd) = even number
-        
-        for(int i = 0 ; i < nums1.size() ; i++){
-            int x = nums1[i];
-
-            if(x % 2 == 0){
-                all_odd &= (x - smallest_odd >= 1);
-            } 
-
-            else{
-                all_even &= (x - smallest_odd >= 1);
-            } 
+        if(mini % 2 == 0){
+            return false || !odd_occured;
         }
 
-        return all_odd || all_even;
+        return true;
+
+        // bool all_odd = true , all_even = true;
+
+        
+        // //all odd try by subtract smallest even as (even - odd) = (odd - even) = odd number
+
+        // //all even try by subtract smallest even if num is even or subtract odd if num is odd as (even - even) = (odd - odd) = even number
+        
+        // for(int i = 0 ; i < nums1.size() ; i++){
+        //     int x = nums1[i];
+
+        //     if(x % 2 == 0){
+        //         all_odd &= (x - smallest_odd >= 1);
+        //     } 
+
+        //     else{
+        //         all_even &= (x - smallest_odd >= 1);
+        //     } 
+        // }
+
+        // return all_odd || all_even;
     }
 };
