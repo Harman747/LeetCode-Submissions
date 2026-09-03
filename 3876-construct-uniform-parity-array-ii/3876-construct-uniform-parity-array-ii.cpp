34051ad1b7a67e7f3bc5c69d1fa36cedb8fe1491
Harman747 <harman.cu12@gmail.com>
2026-09-03 21:57:52 +0530

    Time: 7 ms (51.05%), Space: 165.8 MB (62.83%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -13,19 +13,17 @@ public:
 
         
         //all odd try by subtract smallest even as (even - odd) = (odd - even) = odd number
+
+        //all even try by subtract smallest even if num is even or subtract odd if num is odd as (even - even) = (odd - odd) = even number
+        
         for(int i = 0 ; i < nums1.size() ; i++){
             int x = nums1[i];
 
             if(x % 2 == 0){
                 all_odd &= (x - smallest_odd >= 1) ? true : false;
             } 
-        }
 
-        //all even try by subtract smallest even if num is even or subtract odd if num is odd as (even - even) = (odd - odd) = even number
-        for(int i = 0 ; i < nums1.size() ; i++){
-            int x = nums1[i];
-
-            if(x % 2 != 0){
+            else{
                 all_even &= (x - smallest_odd >= 1) ? true : false;
             } 
         }
