045eca4cf6d5c4f74eb9193aa157ad13540ff23f
Harman747 <harman.cu12@gmail.com>
2026-09-08 12:21:46 +0530

    Time: 0 ms (100%), Space: 8.6 MB (15.51%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,19 +1,7 @@
 class Solution {
 public:
     int countCommas(int n) {
-        if(n < 1000) return 0;
-
-        int base = 1000 , ans = 1 , comma_count = 1;
-
-        while(base * 1000 < n){
-            ans += (base * comma_count);
-            base *= 1000;
-            comma_count++;
-        }
-
-        ans += (abs(base - n) * comma_count);
-
-        return ans;
-
+        if(n < 999) return 0;
+        return n - 999;
     }
 };
