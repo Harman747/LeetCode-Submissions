f3f22259dd91d4da4934b5c77abdcff1a1485d8f
Harman747 <harman.cu12@gmail.com>
2026-09-10 22:48:45 +0530

    Time: 832 ms (38.74%), Space: 428.6 MB (33.42%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
@@ -3,62 +3,63 @@ public:
     vector<int> countSmaller(vector<int>& nums) {
         int n = nums.size();
         vector<int> ans(n, 0);
+        vector<int> idx(n);
 
-        vector<pair<int, int>> arr;
+        for (int i = 0; i < n; i++)
+            idx[i] = i;
 
-        for (int i = 0; i < n; i++) {
-            arr.push_back({nums[i], i});
-        }
-
-        mergeSort(arr, 0, n - 1, ans);
+        mergeSort(nums, idx, ans, 0, n - 1);
 
         return ans;
     }
 
-    void mergeSort(vector<pair<int, int>>& arr, int left, int right,
-                   vector<int>& ans) {
-        
-        if (left >= right)
+    void mergeSort(vector<int>& nums, vector<int>& idx,
+                   vector<int>& ans, int l, int r) {
+        if (l >= r)
             return;
 
-        int mid = left + (right - left) / 2;
+        int mid = l + (r - l) / 2;
 
-        mergeSort(arr, left, mid, ans);
-        mergeSort(arr, mid + 1, right, ans);
+        mergeSort(nums, idx, ans, l, mid);
+        mergeSort(nums, idx, ans, mid + 1, r);
 
-        vector<pair<int, int>> temp;
+        merge(nums, idx, ans, l, mid, r);
+    }
 
-        int i = left;
+    void merge(vector<int>& nums, vector<int>& idx,
+               vector<int>& ans, int l, int mid, int r) {
+
+        vector<int> temp;
+        int i = l;
         int j = mid + 1;
-        int smaller = 0;
+        int rightSmaller = 0;
 
-        while (i <= mid && j <= right) {
+        while (i <= mid && j <= r) {
 
-            if (arr[j].first < arr[i].first) {
-                temp.push_back(arr[j]);
-                smaller++;
+            if (nums[idx[j]] < nums[idx[i]]) {
+                temp.push_back(idx[j]);
+                rightSmaller++;
                 j++;
             }
             else {
-                ans[arr[i].second] += smaller;
-                temp.push_back(arr[i]);
+                ans[idx[i]] += rightSmaller;
+                temp.push_back(idx[i]);
                 i++;
             }
         }
 
         while (i <= mid) {
-            ans[arr[i].second] += smaller;
-            temp.push_back(arr[i]);
+            ans[idx[i]] += rightSmaller;
+            temp.push_back(idx[i]);
             i++;
         }
 
-        while (j <= right) {
-            temp.push_back(arr[j]);
+        while (j <= r) {
+            temp.push_back(idx[j]);
             j++;
         }
 
-        for (int k = 0; k < temp.size(); k++) {
-            arr[left + k] = temp[k];
-        }
+        for (int k = 0; k < temp.size(); k++)
+            idx[l + k] = temp[k];
     }
 };
