259069540859d7e944a875fdf0feb103d6965bcc
Harman747 <harman.cu12@gmail.com>
2026-09-11 14:48:58 +0530

    Time: 21 ms (17.86%), Space: 33.3 MB (15.18%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -7,26 +7,18 @@ public:
         for(int i = 0 ; i < digits.size() ; i++){
             
             if(digits[i] == 0) continue;
-            string cur_s = "";
-
-            cur_s.push_back(digits[i]);
 
             for(int j = 0 ; j < digits.size() ; j++){
             
                 if(j == i) continue;
-                
-                cur_s.push_back(digits[j]);
 
                 for(int k = 0 ; k < digits.size() ; k++){
             
                     if(k == i || k == j) continue;
                     if(digits[k] % 2 != 0) continue;
-
-                    cur_s.push_back(digits[k]);
+                    string cur_s = to_string(digits[i]) + to_string(digits[j]) + to_string(digits[k]);
                     st.insert(cur_s);
-                    cur_s.pop_back();
                 }
-                cur_s.pop_back();
             }
         }
         return st.size();
